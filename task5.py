cb1cf95ae76c84c56c4508d8c58dcfd63eedb383
ilakkiya2001 <56299149+ilakkiya2001@users.noreply.github.com>
2020-11-27 20:41:13 +0530

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,14 @@
+#Task5
+#1
+def num(a,b):    #defining a fuction
+    print("Addition of two numbers:", a+b)
+    print("Subtraction of two numbers:", a-b)
+    print("Multiplication of two numbers:",a*b)
+    print("Division of two numbers:",a/b)
+
+num(10,5)       #calling a function
+#2
+def covid(name,temp):
+    print("Patient name:",name+" "+"Temperature:",temp)
+
+covid("Asha","98")
